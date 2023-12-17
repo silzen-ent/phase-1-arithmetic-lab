4e33d96bd5849560f2a205f9b0f78496d1690dd0
Alan Silva <silzen.ent@gmail.com>
2023-12-17 01:12:27 -0700

    completed arithmetic lab - phase 1

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1; 
+
+let num3 = 60
+let num4 = 7
+const mod = num3 % num4; 
+
+const max = Math.max(5, 10, 15, 20);
